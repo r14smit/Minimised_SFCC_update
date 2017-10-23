770eb580c7b7361fb5648f73a54027999262c725
Rik Smit <rik.smit@varovafashion.nl>
2017-10-23 15:28:30 +0100

    added quickzipall in functions

---
+++ quickZipAll.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include 'functions.php';
+
+$file = 'zip/monthly-catalogs-20171005080001.zip';
+$stock = 'zip/monthly_price-inventory-20171004000002.zip';
+
+foreach ($brandList as $key => $value) {
+ 
+    echo moveCatalog($file,'unzipped/',$value[0],'MASTER');
+    $catalog = $value[1].'-NAVIGATION';
+    echo moveCatalog($file,'unzipped/',$value[0],$catalog);
+
+    echo moveCatalog($stock,'unzipped/',$value[0],'inventory-lists');
+    $pricebook = $value[0].'-eur-list';
+    echo moveCatalog($stock,'unzipped/',$pricebook,'pricebooks');
+   
+}    
+?>
+
